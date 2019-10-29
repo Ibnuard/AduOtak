eebe6b44ba68f1365731547f6c9c3a1e41959403
Ibnu Putra <azxarblog@gmail.com>
2019-10-29 07:46:55 +0700

    Firebase Included

---
+++ app/src/main/java/com/ibnuputra/classchallenges/JoinRoomActivity.java
@@ -0,0 +1,14 @@
+package com.ibnuputra.classchallenges;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_room);
+    }
+}
